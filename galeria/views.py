259ef02319f289e216bd467a7467210b9456fc2f
vToshio <vtoshio2004@gmail.com>
2025-02-16 01:56:48 -0300

    Realizada a criação de um modelo de Fotografia e feita a migração desse modelo para o banco de dados.

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+        1: {
+            'nome': 'Nebulosa de Carina',
+            'legenda': 'webbtelescope.org / Nasa / James Webb'
+        },
+        2: {
+            'nome': 'Galáxia NGC 1079',
+            'legenda': 'nasa.org / Nasa / Hubble'
+        }
+    }
+
+    return render(request, 'galeria/index.html', {'cards': dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
