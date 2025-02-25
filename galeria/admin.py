965595667ec15304304e15f642df5474239c75f8
vToshio <vtoshio2004@gmail.com>
2025-02-25 10:20:01 -0300

    Ajuste na organização de arquivos e front-end.

--- galeria/admin.py
+++ galeria/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from galeria.models import Fotografia
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'categoria', 'legenda', 'data_fotografia', 'publicada')
+    list_display = ('id', 'nome', 'usuario', 'categoria', 'legenda', 'data_fotografia', 'publicada')
     list_display_links = ('id', 'nome')
     list_filter = ('categoria', 'usuario')
     list_editable = ('publicada',)
--- setup/settings.py
+++ setup/settings.py
@@ -143,3 +143,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Messages 
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success'
+}
